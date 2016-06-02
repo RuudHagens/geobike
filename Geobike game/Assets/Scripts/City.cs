4bd057a427e0a8c27fda67a0b5d13df98f70c7f8
jesse deij <jessedeij@hotmail.com>
2016-06-02 14:41:06 +0200

    tweede map + klasses
    tweede map toegevoegd en de klasses aangemaakt met een constructor(lege)

---
+++ Geobike game/Assets/Scripts/City.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class City : MonoBehaviour
+{
+
+    public City()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
---
+++ Geobike game/Assets/Scripts/Mapmanager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mapmanager : MonoBehaviour
+{
+
+    public Mapmanager()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
