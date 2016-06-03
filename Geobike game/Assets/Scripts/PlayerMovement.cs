830d8257c8611112002fb3fe9a25db0bf35cca0b
Daphne van der Veeke <daphnevdveeke@gmail.com>
2016-06-03 11:07:30 +0200

    Movement
    Snelheden zijn nog niet helemaal eerlijk

--- Geobike game/Assets/Scripts/PlayerMovement.cs
+++ Geobike game/Assets/Scripts/PlayerMovement.cs
@@ -1,61 +1,150 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0f;
     public Collider2D collider;
-    private Rigidbody2D rb;
+    private bool up;
+    public bool player1;
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();
+	    up = true;
 	}
 
     //public float speed = 1.5f;
 
     void Update()
     {
-        if (speed > 0f)
+        if (player1)
         {
-            speed = speed - 0.05f;
-        }
+            if (up)
+            {
+                if (speed > 0f)
+                {
+                    speed = speed - 0.05f;
+                }
 
-        if (speed < 0f)
-        {
-            speed = 0f;
-        }
+                if (speed < 0f)
+                {
+                    speed = 0f;
+                }
 
-        transform.position += Vector3.up * speed * Time.deltaTime;
+                transform.position += Vector3.up*speed*Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.position += Vector3.left*speed*Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.position += Vector3.right*speed*Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    speed = speed + 0.15f;
+                }
+            }
+            else
+            {
+                if (speed < 0f)
+                {
+                    speed = speed + 0.05f;
+                }
+
+                if (speed > 0f)
+                {
+                    speed = 0f;
+                }
+
+                transform.position += Vector3.up*speed*Time.deltaTime;
+
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.position += Vector3.left*speed*Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.position += Vector3.right*speed*Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    speed = speed - 0.15f;
+                }
+            }
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            speed = speed + 0.15f;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            if (up)
+            {
+                if (speed > 0f)
+                {
+                    speed = speed - 0.05f;
+                }
+
+                if (speed < 0f)
+                {
+                    speed = 0f;
+                }
+
+                transform.position += Vector3.up * speed * Time.deltaTime;
+
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position += Vector3.left * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += Vector3.right * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    speed = speed + 0.15f;
+                }
+            }
+            else
+            {
+                if (speed < 0f)
+                {
+                    speed = speed + 0.05f;
+                }
+
+                if (speed > 0f)
+                {
+                    speed = 0f;
+                }
+
+                transform.position += Vector3.up * speed * Time.deltaTime;
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += Vector3.left * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position += Vector3.right * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    speed = speed - 0.15f;
+                }
+            }
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
-        //if (other.gameObject.CompareTag("Pick Up"))
-        //{
-        //    other.gameObject.SetActive(false);
-        //    count = count + 1;
-        //    SetCountText();
-        //}
-
-        Quaternion quat = transform.rotation;
-        quat.z = 180;
-        transform.rotation = quat;
+        if (other.gameObject.CompareTag("Collider"))
+        {
+            if (up)
+            {
+                up = false;
+            }
+            else
+            {
+                up = true;
+            }
+        } 
     }
 }
