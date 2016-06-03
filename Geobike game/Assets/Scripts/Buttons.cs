20d2e84cfec475c219ae8099b22b7faa6b04a1de
Daphne van der Veeke <daphnevdveeke@gmail.com>
2016-06-03 15:07:27 +0200

    Begin en prescene toevoegoed
    Prescene nog niet geïmplementeerd

---
+++ Geobike game/Assets/Scripts/Buttons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    // Method to load the main scene
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("pre scene");
+    }
+
+    // Method to exit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
