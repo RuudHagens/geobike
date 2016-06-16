7130e930cf2b0a250f74e92d30cf68acc0ed67ef
Hagens,Ruud R <ruud.hagens@hotmail.com>
2016-06-16 15:02:44 +0200

    Placenames now working

---
+++ Geobike game/Assets/Scripts/PlaceNameLayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceNameLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        foreach(Transform t in GetComponentInChildren<Transform>())
+        {
+            t.gameObject.GetComponent<Renderer>().sortingLayerName = "Names";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
