ee665c915c889c25f2dbeeaa0f540a2fb9ad23d9
Hagens,Ruud R <ruud.hagens@hotmail.com>
2016-06-16 14:31:42 +0200

    created placenames

---
+++ Geobike game/Assets/Scripts/SortPlacenames.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortPlacenames : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingLayerName = "Names";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
