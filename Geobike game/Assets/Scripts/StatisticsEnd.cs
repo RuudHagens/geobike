dd0d47b7a07bb24c50441f68e4473f56aea7223a
Hagens,Ruud R <ruud.hagens@hotmail.com>
2016-06-29 19:58:20 +0200

    end screen stuff

---
+++ Geobike game/Assets/Scripts/StatisticsEnd.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StatisticsEnd : MonoBehaviour {
+
+    private Dictionary<string, float> listOfFacts;
+
+    // Use this for initialization
+    void Start () {
+        listOfFacts = new Dictionary<string, float>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
