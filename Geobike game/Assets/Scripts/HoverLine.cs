ac071985cde795fd57a690fd9faceda6bd218e16
Daphne van der Veeke <daphnevdveeke@gmail.com>
2016-06-29 13:48:50 +0200

    Hoveren zit in end scene

---
+++ Geobike game/Assets/Scripts/HoverLine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverLine : MonoBehaviour {
+
+    public GameObject fastestLineRenderer;
+    public GameObject player1LineRenderer;
+    public GameObject player2LineRenderer;
+
+    public void EnableFastest()
+    {
+        fastestLineRenderer.GetComponent<LineRenderer>().enabled = true;
+        player1LineRenderer.GetComponent<LineRenderer>().enabled = false;
+        player2LineRenderer.GetComponent<LineRenderer>().enabled = false;
+    }
+
+    public void EnablePlayer1()
+    {
+        fastestLineRenderer.GetComponent<LineRenderer>().enabled = false;
+        player1LineRenderer.GetComponent<LineRenderer>().enabled = true;
+        player2LineRenderer.GetComponent<LineRenderer>().enabled = false;
+    }
+
+    public void EnablePlayer2()
+    {
+        fastestLineRenderer.GetComponent<LineRenderer>().enabled = false;
+        player1LineRenderer.GetComponent<LineRenderer>().enabled = false;
+        player2LineRenderer.GetComponent<LineRenderer>().enabled = true;
+    }
+
+    public void ShowAll()
+    {
+        fastestLineRenderer.GetComponent<LineRenderer>().enabled = true;
+        player1LineRenderer.GetComponent<LineRenderer>().enabled = true;
+        player2LineRenderer.GetComponent<LineRenderer>().enabled = true;
+    }
+}
